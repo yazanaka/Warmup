51286455dac93054506702dc1ade7a284a251685
yazanaka <yznwwk1@gmail.com>
2019-03-27 09:32:46 +0200

    this is the anwser of the last warm up

--- lastWarmup.js
+++ lastWarmup.js
@@ -6,5 +6,16 @@ Example:
 var nums = [2, 7, 11, 15], target = 9,
 twoSum(nums,9); //should return [0,1] because nums[0] + nums[1] = 2 + 7 = 9;
 */
-
+function twoSum(arr,terget){
+	var result = []
+	for (var i = 0 ; i < arr.length; i++) {
+	 for(var j = i+1; j < arr.length; j++){
+	 	if(arr[i] + arr[j] === terget){
+	 	 result.push(i)
+	 	 result.push(j)
+	 	 break;
+	 	}
+	 }
+	} return result
+}
 
